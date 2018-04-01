c6ee049813346708538e0b8272d58b24a18bfa9b
Enigma-Games <37983362+Enigma-Games@users.noreply.github.com>
2018-04-01 17:25:13 -0400

    Added commands 'kick' and 'ban'
    More on the way!

--- main.py
+++ main.py
@@ -9,9 +9,38 @@ import datetime
 Client = discord.Client()
 client = commands.Bot(command_prefix = "E", description="E is the prefix.")
 
+@client.event
+async def on_command_error(ctx, error:discord.ext.commands.CommandError):
+    errorid = False
+    hasargs = False
+    if isinstance(error,commands.MissingRequiredArgument):
+        tprint("User did not provide enough arguments:")
+        scui(ctx.guild,ctx.channel,ctx.author)
+        errorid = True
+        hasargs = True
+        await client.ctx.send_message(ctx.message.channel, ":warning: **You haven't provided enough arguments!**.")
+
+
 
 @client.command(pass_context=True)
 async def ping(ctx):
-    await client.say("hello world")
+    await client.say("{} Pong!".format(ctx.message.author.mention))
 
-client.run("Bot token")
+@client.command(pass_context=True)
+async def kick(ctx, userName : discord.User):
+    await client.kick(userName)
+    await client.say("{} has been kicked. :white_check_mark:".format(userName))
+
+
+@client.command(pass_context=True)
+async def ban(ctx, userName : discord.User):
+    await client.ban(userName)
+    await client.say("{} has been banned. :white_check_mark:".format(userName))
+        
+        
+    
+
+    
+
+    
+client.run("Bot Token")
