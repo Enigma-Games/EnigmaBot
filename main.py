b6226dcbcb169d4f3b74677dcdf12491c8654a38
Enigma-Games <37983362+Enigma-Games@users.noreply.github.com>
2018-04-08 10:28:03 -0400

    Fixed Several Major Issues + Added Evo Stone
    -Improved Pokemon Pokedex

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ Client = discord.Client()
 client = commands.Bot(command_prefix = "E", description="E is the prefix.")
 pokes = pokedex.Pokedex(version="V1", user_agent="TheEBBGaming#4633 and Futuristick#7622")
 
-
-
 @client.event
 async def on_ready():
     print("ENIGMA'S AUTOMATION IS READY TO ROLL!")
@@ -78,13 +76,20 @@ async def unban(ctx, userName : discord.User):
 async def pokedex(ctx, theString : str):
     pokemon_name = pokes.get_pokemon_by_name(theString)
     for form in pokemon_name:
-        embedPokedex = discord.Embed(title=form['name'] + " " + form['number'], description="*The " + form['species'] + " Pokémon*", color=0x7289da)
+        embedPokedex = discord.Embed(title=form['name'] + " (" + form['number'] + ")", description="*The " + form['species'] + " Pokémon*", color=0x7289da)
         embedPokedex.add_field(name="Types:", value=", ".join(form['types']), inline=False)
+        embedPokedex.add_field(name="Height:", value=form['height'], inline=False)
+        embedPokedex.add_field(name="Weight:", value=form['weight'], inline=False)
+        embedPokedex.add_field(name="Evolution Line:", value=" -> ".join(form['family']['evolutionLine']), inline=False)
+        embedPokedex.add_field(name="Egg Groups:", value=", ".join(form['eggGroups']), inline=False)
+        embedPokedex.set_thumbnail(url=form['sprite'])
         await client.say(embed=embedPokedex)
+        
     
 
 @client.command(pass_context=True)
 async def pokedex2(ctx, theString : str):
+    pokemon_name = pokes.get_pokemon_by_name(theString)
     if theString == 'categories':
         categories = ["Starter","Legendary","Mythical","Ultra Beast","Mega"]
         await client.say(", ".join(categories))
@@ -101,6 +106,26 @@ async def pokedex2(ctx, theString : str):
         await bot.say(" ".join(pokemon_name))
     else:
         await client.say(":warning: Not found in Pokedex. Please note that all `pokedex` commands are case-sensitive. Do `Epokedex help` for more info.")
+
+
+@client.command(pass_context=True)
+async def pokedex3(ctx, theString : str):
+    league = pokes.get_league(theString)
+    embedPokedex = discord.Embed(title=league['name'], description="The " + league['region'] + " Region", color=0x7289da)
+    embedPokedex.add_field(name="Badges:", value=", ".join(league['badges']), inline=False)
+    embedPokedex.add_field(name="Badges Required:", value=league['badgesRequired'], inline=False)
+    await client.say(embed=embedPokedex)
+
+@client.command(pass_context=True)
+async def pokedex4(ctx, theString : str):
+    evostone = pokes.get_evolution_stone(theString)
+    embedPokedex = discord.Embed(title=evostone['name'], description="aka " + evostone['aka'], color=0x7289da)
+    embedPokedex.add_field(name="Effects:", value="\n".join(evostone['effects']), inline=False)
+    embedPokedex.set_thumbnail(url=evostone['sprite'])
+    await client.say(embed=embedPokedex)
+    
+
+
         
         
     
